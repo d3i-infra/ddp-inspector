299805e8f4b5773d814b866e2973edfa3f053b9f
trbKnl <ncdeschipper@gmail.com>
2022-09-20 14:40:11 +0200

    tests for parserlib

--- ddpinspect/tests/test_stringparse.py
+++ ddpinspect/tests/test_stringparse.py
@@ -1,6 +1,11 @@
-import pytest
+"""
+Tests to determine the functionality of functions in the parserlib module
 
+Tests can be added and changed when needed 
+"""
+import pytest
 from parserlib import stringparse
+
 
 @pytest.mark.parametrize("test_timestamp, expected", [
     ("2005-10-30 T 10:45 UTC", True),
@@ -58,12 +63,62 @@ def test_has_url(test_url: str, is_exact_url: bool, expected: bool) -> None:
         f"url: {test_url} in exact mode: {is_exact_url}, it should be bool: {expected}"
 
 
-has_email(input_string: str, exact: bool = False) -> bool:
-#
-#def is_ipaddress(input_string: str) -> bool:
+
+@pytest.mark.parametrize("test_email, is_exact_email, expected", [
+    # These should evaluate to True 
+    (r'simple@example.com', True, True),
+    (r'very.common@example.com', True, True),
+    (r'disposable.style.email.with+symbol@example.com', True, True),
+    (r'other.email-with-hyphen@example.com', True, True),
+    (r'fully-qualified-domain@example.com', True, True),
+    (r'user.name+tag+sorting@example.com', True, True),
+    (r'x@example.com', True, True),
+    (r'example-indeed@strange-example.com', True, True),
+    (r'test/test@test.com', True, True),
+    (r'example@s.example', True, True),
+    (r'"john..doe"@example.org', True, True),
+    (r'mailhost!username@example.org', True, True),
+    (r'"very.(), :;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com', True, True),
+    (r'user%example.com@example.org', True, True),
+    (r'user-@example.org', True, True),
+    (r'postmaster@[123.123.123.123]', True, True),
+    # These should evaluate to True, but the current implementation evaluate them to False
+    (r'postmaster@[IPv6:2001:0db8:85a3:0000:0000:8a2e:0370:7334]', True, False),
+    (r'admin@mailserver1', True, False),  
+    (r'""@example.org ', True, False),     
+    # These should evaluate to False
+    (r'Abc.example.com', True, False),
+    ('A@b@c@example.co', True, False),
+    (r'a"b(c)d,e:f;g<h>i[j\k]l@example.co', True, False),
+    (r'just"not"right@example.co', True, False),
+    (r'this is"not\allowed@example.co', True, False),
+    (r'this\ still\"not\\allowed@example.co', True, False),
+    (r'i_like_underscore@but_its_not_allowed_in_this_part.example.co', True, False),
+    # These should evaluate to False, but the current implementation evaluate them to True
+    (r'1234567890123456789012345678901234567890123456789012345678901234+x@example.co', True, True)
+])
+def test_has_email(test_email: str, is_exact_email: bool, expected: bool) -> None:
+    """
+    test stringparse.has_url
+
+    Note a perfect regex does not exist.
+    Determine whether the outcome of these test yourself
+    """
+    assert stringparse.has_email(test_email, is_exact_email) is expected,\
+        f"email: {test_email} in exact mode: {is_exact_email}, it should be bool: {expected}"
 
 
-
-# geolocatie
-# telnummer
-# is path vs has path
+@pytest.mark.parametrize("test_ip, expected", [
+    ("127.0.0.1",     True),
+    ("1788727.0.0.1", False),
+    ("127.0.0.256",   False),
+    ("2001:db8:3333:4444:5555:6666:7777:8888", True),
+    ("2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF", True),
+    ("2001:db8:3333:4444:CQCC:DDDD:EEEE:FFFF", False),
+])
+def test_is_ipaddress(test_ip: str, expected: bool) -> None:
+    """
+    test stringparse.has_ip
+    """
+    assert stringparse.is_ipaddress(test_ip) is expected,\
+        f"ip: {test_ip}, it should be bool: {expected}"
